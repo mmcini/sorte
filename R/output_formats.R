b6c85bf5e1ddc17d581fb32b7cb86243206905d3
M Mancini <mrcl.mancini@gmail.com>
2021-10-11 02:46:23 -0300

    initial commit

---
+++ R/output_formats.R
@@ -0,0 +1,33 @@
+#' Defines the formats for the outputs of each template
+#'
+#' Functions provide the templates for documents
+#'
+#' @param \dots Arguments for the *_document functions
+#'
+#' @import rmarkdown
+#'
+#' @author M Mancini
+#' @name formats
+NULL
+
+#' @export
+#' @rdname formats
+minimal_template <- function(...) {
+    template <- system.file(
+                "rmarkdown/templates/minimal-template/src/minimaltemplate.tex",
+                package = "Sorte"
+    )
+
+    pdf_document(..., template = template)
+}
+
+#' @export
+#' @rdname formats
+tiny_template <- function(...) {
+    template <- system.file(
+                "rmarkdown/templates/tiny-template/src/tinytemplate.tex",
+                package = "Sorte"
+    )
+
+    pdf_document(..., template = template)
+}
